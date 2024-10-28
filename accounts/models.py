ac9279a06353816d63bf82931d75037db15c5749
Solotonics <solomonokuneye1@gmail.com>
2024-10-28 14:07:49 -0700

    UserProfile Signal created

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -25,3 +27,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}--profile'
+    
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, created, instance, **kwargs):
+    if created:
+        Profile.objects.create(user=instance) 
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    Profile.objects.get(user=instance).save()
