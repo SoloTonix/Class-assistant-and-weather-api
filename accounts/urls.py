34ab4bf80637ee9362cd5809a12c2c8cace96178
Solotonics <solomonokuneye1@gmail.com>
2024-10-28 05:16:20 -0700

    User profile detail view completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
+    path('profile/', UserProfile, name='UserProfile'),
     path('login/',Login, name='Login'),
     path('logout/', Logout, name='Logout'),
     path('register/', Register, name='Register'),
